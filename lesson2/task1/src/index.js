2ba62a49031314bc7998764aea1b266d121d7eae
JeePik <andrewlegeza@gmail.com>
2022-06-30 17:05:43 +0300

    added files lesson2 task1

---
+++ lesson2/task1/src/index.js
@@ -0,0 +1,22 @@
+import { createStore } from "redux";
+
+export const increment = {
+  type: "COUNTER/INCREMENT",
+};
+
+export const decrement = {
+  type: "COUNTER/DECREMENT",
+};
+
+export const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+export const store = createStore(counterReducer);
